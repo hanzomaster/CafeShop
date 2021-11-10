c26d30e694ff8f3a4b217669ccf0a1f6d65d4d97
Thinh Tran <thinhrom11@gmail.com>
2021-11-10 23:10:05 +0700

    Change FXML variables to private

--- src/frontend/ScreenProperty.java
+++ src/frontend/ScreenProperty.java
@@ -3,6 +3,7 @@ package frontend;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -12,36 +13,58 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
 public class ScreenProperty implements Initializable {
-
-  public AnchorPane menuPane;
-  public TextArea observablTextArea;
-  public TextField DishesText;
-  public TextField numberofPiece;
-  public ListView menuView;
-  public TextArea BillArea;
-  public Label dishes;
-  public Label number;
-  public Label bill;
-  public Button table1;
-  public Button table2;
-  public Button table3;
-  public Button table4;
-  public Button table5;
-  public Button table6;
-  public Button table7;
-  public Button table8;
-  public Button table9;
-  public Button table10;
-  public Button table11;
-  public Button table12;
-  public Button addButton;
-  public Button orderedButton;
-  public Button tableAvail[] = new Button[13];
+  @FXML
+  private AnchorPane menuPane;
+  @FXML
+  private TextArea observablTextArea;
+  @FXML
+  private TextField DishesText;
+  @FXML
+  private TextField numberofPiece;
+  @FXML
+  private ListView menuView;
+  @FXML
+  private TextArea BillArea;
+  @FXML
+  private Label dishes;
+  @FXML
+  private Label number;
+  @FXML
+  private Label bill;
+  @FXML
+  private Button table1;
+  @FXML
+  private Button table2;
+  @FXML
+  private Button table3;
+  @FXML
+  private Button table4;
+  @FXML
+  private Button table5;
+  @FXML
+  private Button table6;
+  @FXML
+  private Button table7;
+  @FXML
+  private Button table8;
+  @FXML
+  private Button table9;
+  @FXML
+  private Button table10;
+  @FXML
+  private Button table11;
+  @FXML
+  private Button table12;
+  @FXML
+  private Button addButton;
+  @FXML
+  private Button orderedButton;
+  @FXML
+  private Button[] tableAvail = new Button[13];
 
 
   @Override
   public void initialize(URL arg0, ResourceBundle arg1) {
-    // TODO Auto-generated method stub
     tableAvail[1] = table1;
     tableAvail[2] = table2;
     tableAvail[3] = table3;
@@ -72,9 +95,7 @@ public class ScreenProperty implements Initializable {
     for (int i = 1; i <= 12; i++) {
       tableAvail[i].setVisible(true);
       int set = i;
-      tableAvail[i].setOnAction(e -> {
-        tableAvail[set].setText("Occupied");
-      });
+      tableAvail[i].setOnAction(e -> tableAvail[set].setText("Occupied"));
     }
     DishesText.setVisible(false);
     numberofPiece.setVisible(false);
